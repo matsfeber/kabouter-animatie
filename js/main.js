0a32dd6212713d66a7b80ba9f10c976406bb96ff
Mats de Feber <36168490+matsfeber@users.noreply.github.com>
2020-04-09 10:14:19 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,49 @@
+window.onclick =function playIntro(){
+    document.getElementById("intro").play();
+    document.getElementById("achtergrondMuziek").play();
+    this.onclick=null;
+}
+
+
+document.getElementById("button1").onclick = function button1() {
+ setVideo1();
+
+  document.getElementById("video").pause();
+  document.getElementById("video").classList.remove("hidden");
+  document.getElementById("button2").style.backgroundColor = null;
+  document.getElementById("button3").style.backgroundColor = null;
+};
+
+function setVideo1() {
+  document.getElementById("video").src = "./img/anim1.mp4";
+  setTimeout(playVideo, 1);
+}
+
+function playVideo() {
+  document.getElementById("video").play();
+}
+
+document.getElementById("button2").onclick = function button2() {
+  setVideo2();
+
+  document.getElementById("video").pause();
+  document.getElementById("video").classList.remove("hidden");
+};
+
+function setVideo2() {
+  document.getElementById("video").src = "./img/anim2.mp4";
+  setTimeout(playVideo, 1);
+}
+
+
+
+document.getElementById("button3").onclick = function button3() {
+  setVideo3();
+  document.getElementById("video").pause();
+
+};
+
+function setVideo3() {
+  document.getElementById("video").src = "./img/anim3.mp4";
+  setTimeout(playVideo, 1);
+}
